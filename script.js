9d190cb473330366585892ce1121a31d1dfdb7dd
Joel Yap <joelytk@gmail.com>
2024-09-17 22:39:41 +0800

    Removed pdf menu for now

--- script.js
+++ script.js
@@ -2,24 +2,6 @@ const tree = document.getElementById('tree');
 
 const fetchData = async () => {
   try {
-    const previewPdf = () => {
-      FamilyTree.pdfPrevUI.show(familyTree, {
-        format: 'A4',
-        header: '',
-        footer: '',
-        margin: [200, 16, 200, 16],
-        landscape: true
-      });
-    };
-
-    const downloadPdf = () => {
-      familyTree.exportPDF({
-        format: 'A4',
-        padding: 16,
-        landscape: true
-      });
-    };
-
     const res = await fetch('./data.json');
     const data = await res.json();
     const familyTree = new FamilyTree(tree, {
@@ -37,21 +19,8 @@ const fetchData = async () => {
         zoom: true,
         fit: true
       },
-      menu: {
-        pdfPreview: {
-          text: 'Preview PDF',
-          icon: FamilyTree.icon.pdf(24, 24, '#7A7A7A'),
-          onClick: previewPdf
-        },
-        export_pdf: {
-          text: 'Export PDF',
-          icon: FamilyTree.icon.pdf(24, 24, '#7A7A7A'),
-          onClick: downloadPdf
-        }
-      },
       nodes: data
     });
-    // familyTree.load(data);
     familyTree.onNodeClick(() => false);
   } catch (error) {
     console.error({ error });
